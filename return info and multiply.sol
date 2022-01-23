20a0021d18f37af4d5bf786477b4b822cf072d8c
Slava <80632445+DemaPy@users.noreply.github.com>
2022-01-23 18:47:59 +0100

    Update return info and multiply.sol

--- return info and multiply.sol
+++ return info and multiply.sol
@@ -1,11 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.8.7;
 
-
+//You can use 1 function to return it all
 contract AboutMe {
     uint to_multiply = 2;
 
+    function multiply(uint x) public view returns (uint) {
+        return x * num_to_mulyiply
+    }
+
     function user__info__multiply(uint value) public view returns (string memory firstname, string memory lastname, uint number, uint age, uint result) {
         return (firstname = "Dema", lastname = "Bender", number = 7, age = 23, result = value * to_multiply);
+    }
+}
+
+//Or even devide it on 2 func
+contract AboutMe {
+    uint to_multiply = 2;
+
+    function multiply(uint x) public view returns (uint) {
+        return x * num_to_mulyiply
+    }
+
+    function MyInfo() public pure returns (string memory second_firstname, string memory second_lastname, uint second_age, uint second_number) {
+        second_firstname = "Viacheslav";
+        second_lastname = "Demchenko";
+        second_age = 23;
+        second_number = 7;
     }
 }
