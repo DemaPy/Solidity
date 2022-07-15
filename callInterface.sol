ef5d109e2fa36b54f6c926150eac63e7924dd047
Viacheslav <80632445+DemaPy@users.noreply.github.com>
2022-07-15 19:11:40 +0200

    Update callInterface.sol

--- callInterface.sol
+++ callInterface.sol
@@ -11,3 +11,19 @@ contract CallCounter {
         count = ICounter(_counter).count();
     }
 }
+
+
+*/interface ICounter {
+    function count() external view returns (uint);
+    function inc() external;
+}
+
+contract MyContract {
+    function incCounter(address _counter) external {
+        ICounter(_counter).inc();
+    }
+
+    function getCount(address _counter) external view returns (uint) {
+        return ICounter(_counter).count();
+    }
+}/*
